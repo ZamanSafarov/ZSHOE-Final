17630184d979971f90ffde400fea7c161fc16dcc
Zaman Safarov <zamanjs@code.edu.az>
2023-01-02 21:29:46 +0400

    some changes

--- ZSHOE/Startup.cs
+++ ZSHOE/Startup.cs
@@ -20,24 +20,16 @@ namespace ZSHOE.WebUI
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
@@ -46,11 +38,22 @@ namespace ZSHOE.WebUI
 
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
+            app.UseEndpoints(cfg =>
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                cfg.MapAreaControllerRoute("defaultAdmin", "admin", "admin/{controller=dashboard}/{action=index}/{id?}");
+
+
+                //cfg.MapControllerRoute(
+                //name: "default-accesdenied",
+                //pattern: "accesdenied.html",
+                //defaults: new
+                //{
+                //    area = "",
+                //    controller = "account",
+                //    action = "accesdenied"
+                //});
+
+                cfg.MapControllerRoute("default", "{controller=home}/{action=index}/{id?}");
             });
         }
     }
